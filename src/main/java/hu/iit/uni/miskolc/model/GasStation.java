cb2cd209d0efe5216495dc81885d751e5f610616
david <dervon94@gmail.com>
2018-05-02 17:28:37 +0200

    Create Trip and Gas Stations classes.

--- src/main/java/hu/iit/uni/miskolc/model/GasStation.java
+++ src/main/java/hu/iit/uni/miskolc/model/GasStation.java
@@ -1,6 +1,27 @@
 package hu.iit.uni.miskolc.model;
 
-public class gasStation {
-    private int place;
+public class GasStation {
+    private long place;
+    private long fuelPrice;
 
+    public GasStation(Long place, Long fuelPrice) {
+        this.place = place;
+        this.fuelPrice = fuelPrice;
+    }
+
+    public long getPlace() {
+        return place;
+    }
+
+    public void setPlace(long place) {
+        this.place = place;
+    }
+
+    public long getFuelPrice() {
+        return fuelPrice;
+    }
+
+    public void setFuelPrice(long fuelPrice) {
+        this.fuelPrice = fuelPrice;
+    }
 }
--- src/main/java/hu/iit/uni/miskolc/model/Trip.java
+++ src/main/java/hu/iit/uni/miskolc/model/Trip.java
@@ -1,4 +1,48 @@
 package hu.iit.uni.miskolc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trip {
+    private long tripLength;
+
+    public Trip(long tripLength, long fuelCapacity, long numOfStations, List<GasStation> gasStations) {
+        this.tripLength = tripLength;
+        this.fuelCapacity = fuelCapacity;
+        this.numOfStations = numOfStations;
+        GasStations = gasStations;
+    }
+    public Trip(long tripLength, long fuelCapacity, long numOfStations) {
+        this.tripLength = tripLength;
+        this.fuelCapacity = fuelCapacity;
+        this.numOfStations = numOfStations;
+    }
+
+    private long fuelCapacity;
+    private long numOfStations;
+    private List<GasStation> GasStations=new ArrayList<>();
+
+    public long getTripLength() {
+        return tripLength;
+    }
+
+    public void setTripLength(long tripLength) {
+        this.tripLength = tripLength;
+    }
+
+    public long getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(long fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public long getNumOfStations() {
+        return numOfStations;
+    }
+    public void addGasStation(Long location, Long fuelPrice){
+        GasStations.add(new GasStation(location,fuelPrice));
+
+    }
 }
